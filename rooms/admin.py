bf3251816c938869119cc40361394e18da96ee4e
QuameOphory <quameophory@yahoo.com>
2022-06-19 02:01:26 +0000

    update rooms admin file

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Room
 
 # Register your models here.
+
+@admin.register(Room)
+class RoomsAdmin(admin.ModelAdmin):
+    '''Admin View for Rooms'''
+
+    list_display = ('room_number', 'room_type', 'room_status', 'room_totalbeds', 'room_totalbaths')
+    list_filter = ('room_number',)
+    search_fields = ('room_number',)
